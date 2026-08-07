e834e0bfe5cc2da6836fc9c17ee097d603fab404
Muhammad Banna <muhammadbanna2.71@gmail.com>
2026-08-07 08:35:55 +0700

    Improve production security

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -287,8 +287,6 @@ LOGGING = {
     "disable_existing_loggers": False,
 }
 
-
-
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = (
     "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -297,6 +295,16 @@ STATICFILES_STORAGE = (
 CSRF_TRUSTED_ORIGINS = [
     "https://villageinsightdss-production.up.railway.app",
 ]
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+
+    SECURE_HSTS_SECONDS = 31536000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
 # --------------------------------------------------
 # END
 # --------------------------------------------------
